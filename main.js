fad12981b4e026f53eeae734da2194974bf171af
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-06-27 20:59:34 -0400

    styled div that will hold all content willingly to be ing grid format

---
+++ main.js
@@ -0,0 +1,7 @@
+
+h1 = document.createElement("h1");
+h1.textContent = "Bakery Inventory";
+let body = document.querySelector("body");
+let div = document.querySelector("div");
+body.append(h1);
+body.append(div);
