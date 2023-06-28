d63e9957208de7a5fec2439d05c0767f060f268d
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-06-28 01:11:06 -0400

    made some more js eleements for html, may change topic of inventory app tho

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ let body = document.querySelector("body");
 let div = document.querySelector("div");
 body.append(h1);
 body.append(div);
+let form = document.createElement("form");
+let nameInput = document.createElement("input");
+let fieldset = document.createElement("fieldset");
+let nameLabel = document.createElement("label");
+
+
+nameInput.type = "text";
+nameInput.name = "name";
+
+
+let submitButton = document.createElement("button");
+
+let resetButton = document.createElement("button");
+
+
+
